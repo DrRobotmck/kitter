1010db6b4d754c96bc1f3693a953b64bd73ad13c
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-12 01:07:04 -0400

    hashtag model associations and validations

--- kitter/app/models/hashtag.rb
+++ kitter/app/models/hashtag.rb
@@ -1,2 +1,6 @@
 class Hashtag < ActiveRecord::Base
+  has_and_belongs_to_many :tweets
+
+  validate :content, :num_of_times_used, presence: true
+  validate_numericality_of :num_of_times_used, equal_to: 1 , on: :create
 end
