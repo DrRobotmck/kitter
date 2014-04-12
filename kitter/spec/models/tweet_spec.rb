6d55338619f1415f435e8acb408699d436c11f56
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-12 17:33:20 -0400

    revise tests based on new tweet model associations

--- kitter/spec/models/tweet_spec.rb
+++ kitter/spec/models/tweet_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Tweet do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it {should belong_to :user}
+  it {should have_many :replies}
+  it {should have_many :favorites}
+  it {should have_many :retweets}
+  it {should have_and_belong_to_many :hashtags}
+
+  it {should validate_presence_of :content}
+  it {should validate_presence_of :user_id}
+  it {should validate_presence_of :num_of_favs}
+  it {should validate_presence_of :num_of_retweets}
+
 end
