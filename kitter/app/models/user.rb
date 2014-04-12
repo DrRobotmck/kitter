752d32ab83420f7ff9c6d8034673c185ca93d521
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-12 01:21:16 -0400

    correct blocked_users_users table naming error

--- kitter/app/models/user.rb
+++ kitter/app/models/user.rb
@@ -5,12 +5,13 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :followers,
     foreign_key: 'follower_id',dependent: :destroy
   has_and_belongs_to_many :blocked_users,
-    foreign_key: 'blocked_user_id', dependent: :destroy
+    foreign_key: 'blocked_id', dependent: :destroy
   has_many :replies
   has_many :favorites
   has_many :retweets
 
-  validates :username, :name, :email, :background_photo_url, :profile_photo_url, :bio, :website, :verified, :location, :password, :password_confirmation, :country_id,  presence: true
+  validates :username, :name, :email, :bio, :website, :verified, :location, :password, :password_confirmation, :country_id,  presence: true
+  validates :profile_photo_url, :background_photo_url, presence: true, on: :create
   validates :username, :email, uniqueness: true
   validates :country_id, numericality: {only_integer:true}
   validates :password, length: {minimum: 5}
--- kitter/db/migrate/20140412012322_create_blocked_users_join_table.rb
+++ kitter/db/migrate/20140412012322_create_blocked_users_join_table.rb
@@ -1,6 +1,6 @@
 class CreateBlockedUsersJoinTable < ActiveRecord::Migration
   def change
-    create_join_table :users, :blocked_users do |t|
+    create_join_table :users, :blocked do |t|
       # t.index [:user_id, :blocked_user_id]
       # t.index [:blocked_user_id, :user_id]
     end
