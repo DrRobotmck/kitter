9d409cc321f59e514ac7c909024627046ae8a466
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-12 17:41:46 -0400

    create controllers for user, tweet, hashtag models and for sessions, update application controller for sessions

---
+++ kitter/app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def notifications
+  end
+
+  def followers
+  end
+
+  def following
+  end
+
+  def favorite_tweets
+  end
+
+end
