f72a6032c98257bfc94ae53639122dd1d5b194e2
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-12 17:39:57 -0400

    hashtag model passing tests

--- kitter/spec/models/hashtag_spec.rb
+++ kitter/spec/models/hashtag_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Hashtag do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_and_belong_to_many :tweets}
+  it {should validate_presence_of :content }
+  it {should validate_presence_of :num_of_times_used}
 end
