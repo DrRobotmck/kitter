8fa7d6b11fc0a18a09a975c5409baf6eed530a1c
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-12 17:07:01 -0400

    revise tests based on new user model associations

--- kitter/spec/models/user_spec.rb
+++ kitter/spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it{should have_secure_password}
+  it{should validate_presence_of :name}
+  it{should validate_presence_of :username}
+  it{should validate_presence_of :email}
+  it{should validate_presence_of :background_photo_url}
+  it{should validate_presence_of :profile_photo_url}
+  it{should validate_presence_of :bio}
+  it{should validate_presence_of :website}
+  it{should validate_presence_of :verified}
+  it{should validate_presence_of :location}
+  it{should validate_presence_of :password}
+  it{should validate_presence_of :password_confirmation}
+  it{should validate_presence_of :country_id}
+
+  it{should have_many :tweets}
+  it{should have_and_belong_to_many :followers}
+  it{should have_and_belong_to_many :blocked_users}
+  it{should have_many :replies}
+  it{should have_many :retweets}
+  it{should have_many :favorites}
+  it{should have_many(:retweets).through(:tweets)}
+  it{should have_many(:replies).through(:tweets)}
+  it{should have_many(:favorites).through(:tweets)}
 end
