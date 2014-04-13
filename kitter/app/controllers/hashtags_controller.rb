6dbf42baddd6894715e19194a80854022a4bcb91
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-13 03:01:36 -0400

    add index and show method for hashtag controller

--- kitter/app/controllers/hashtags_controller.rb
+++ kitter/app/controllers/hashtags_controller.rb
@@ -1,8 +1,11 @@
 class HashtagsController < ApplicationController
 
   def index
+    @hashtags = Hashtag.all
   end
 
   def show
+    @hashtag=Hashtag.find_by(content: params[:hashtag])
   end
+
 end
