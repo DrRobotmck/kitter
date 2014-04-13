dccdded6493d53b2bdfdecbe2db608bf4bc8b035
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-12 20:26:38 -0400

    change order of relationships in user model, add new validations

--- kitter/app/models/user.rb
+++ kitter/app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  has_many :tweets, dependent: :destroy
-  has_and_belongs_to_many :followers,
-    foreign_key: 'follower_id', join_table: :followers
-  has_and_belongs_to_many :blocked_users,
-    foreign_key: 'blocked_id', join_table: :blocked_users
 
+  has_many :tweets, dependent: :destroy
   has_many :replies
   has_many :replies, through: :tweets
   has_many :retweets
@@ -14,12 +10,26 @@ class User < ActiveRecord::Base
   has_many :favorites
   has_many :favorites, through: :tweets
 
+  has_and_belongs_to_many :followers,
+    foreign_key: 'follower_id', join_table: :followers
+  has_and_belongs_to_many :blocked_users,
+    foreign_key: 'blocked_id', join_table: :blocked_users
 
   validates :username, :name, :email, :bio, :website, :verified, :location, :password, :password_confirmation, :country_id,  presence: true
   validates :profile_photo_url, :background_photo_url, presence: true, on: :create
-  validates :username, :email, uniqueness: true
-  validates :country_id, numericality: {only_integer:true}
+
+  validates :username, uniqueness: true
+  validates :username, uniqueness: {case_insensitive: true}
+  validates :username, length: {maximum: 15}
+  validates :username, format: {with: /\w/, message: "Usernames may only contain numbers, letters, or underscores."}
+
+  validates :email, uniqueness: true
+  validates :email, uniqueness: {message: "That email is already registered."}
+  validates :email, correct_email_format: true
+
   validates :password, length: {minimum: 5}
+
+  validates :country_id, numericality: {only_integer:true}
   validates :verified, inclusion: { :in => [true, false]}
 
 end
