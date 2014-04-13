f637bfaa2f66d2bbd76e6421e06f4162620c2961
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-13 03:36:39 -0400

    add methods for scanning tweet and extracting hashtags and mentions

--- kitter/app/models/tweet.rb
+++ kitter/app/models/tweet.rb
@@ -21,4 +21,42 @@ class Tweet < ActiveRecord::Base
   end
   # binding.pry
 
+  def scan_tweet(tweet)
+    tweet=find_mentions(tweet)
+    tweet=find_hashtags(tweet)
+    # tweet=find_links(tweet)
+    return tweet
+  end
+
+  def find_media
+    self.content
+  end
+
+
+  def find_mentions(string)
+    string.split.each do |word|
+      if word.match(/@\w{1,}/)
+        user=User.find_by(username: (word.delete("@")))
+        if user
+        string.sub!(word,"<a href='/users/#{user.id}'>#{word}</a>")
+        end
+      end
+    end
+    return string
+  end
+
+  def find_hashtags(string)
+    string.split.each do |word|
+      if word.match(/#\w{1,}/)
+        orig_word=word
+        word=word.delete("#")
+        hashtag=Hashtag.find_by(content: word)
+        hashtag ? hashtag.increase_num_of_times_used : Hashtag.create(content: word)
+        string.sub!(orig_word,"<a href='/hashtags/#{word}'>#{orig_word}</a>")
+     end
+   end
+     return string
+  end
+
+
 end
