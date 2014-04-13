3c83545dfa3130573a2c94001ed08d75dd82bebb
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-13 03:01:13 -0400

    add increase_num_of_times_used method

--- kitter/app/models/hashtag.rb
+++ kitter/app/models/hashtag.rb
@@ -3,4 +3,9 @@ class Hashtag < ActiveRecord::Base
 
   validates :content, :num_of_times_used, presence: true
   validates :num_of_times_used, numericality: {equal_to: 1}, on: :create
+
+  def increase_num_of_times_used
+    update(num_of_times_used: self.num_of_times_used +=1)
+  end
+
 end
