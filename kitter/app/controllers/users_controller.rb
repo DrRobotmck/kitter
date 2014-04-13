80cfa255f56d375fa899a4b02a10aca9d60fc9de
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-13 00:25:48 -0400

    two step user sign-up form, methods for determining current step

--- kitter/app/controllers/users_controller.rb
+++ kitter/app/controllers/users_controller.rb
@@ -1,36 +1,79 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate, only: [:index,:show,:edit,:update,:destroy]
   def index
+    @users=User.all
   end
 
   def show
+    @user=User.find(params[:id])
   end
 
   def new
+    session[:user_values] ||= {}
+    @user=User.new(session[:user_values])
+    @user.current_step=session[:user_step]
   end
 
   def create
+    session[:user_values].deep_merge!(params[:user]) if params[:user]
+    @user=User.new(session[:user_values])
+    @user.current_step=session[:user_step]
+
+    if @user.valid?
+      if params[:back_button]
+        @user.previous_step
+      elsif @user.last_step?
+        @user.save! if @user.valid?
+      else
+        @user.next_step
+      end
+      session[:user_step]=@user.current_step
+    end
+
+    if @user.new_record?
+      render 'new'
+    else
+      session[:user_step] = session[:user_values] = nil
+      session[:user_id] = @user.id
+      redirect_to @user, notice: "User created. Get kitting!"
+    end
+
   end
 
   def edit
+    @user=User.find(params[:id])
   end
 
   def update
+    @user=User.find(params[:id])
+    @user.update(user_params)
   end
 
   def destroy
+    @user=User.find(params[:id])
+    session[@user.id]=nil
+    @user.destroy
   end
 
   def notifications
+    @notifications=@user.notifications
   end
 
   def followers
+    @followers = @user.followers
   end
 
   def following
   end
 
   def favorite_tweets
+    @favorites = @user.favorites
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :name, :email, :bio, :website, :verified, :location, :password, :password_confirmation, :country_id,:profile_photo_url, :background_photo_url)
   end
 
 end
--- kitter/app/models/user.rb
+++ kitter/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
+
   has_secure_password
 
   has_many :tweets, dependent: :destroy
@@ -15,8 +16,10 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :blocked_users,
     foreign_key: 'blocked_id', join_table: :blocked_users
 
-  validates :username, :name, :email, :bio, :website, :verified, :location, :password, :password_confirmation, :country_id,  presence: true
-  validates :profile_photo_url, :background_photo_url, presence: true, on: :create
+  validates :username, :name, :email, :password, :password_confirmation, presence: true
+  validates :bio, :website, :location, :country_id, presence: true, :if => lambda{|user| user.current_step == "additional_info"}
+
+  validates :background_photo_url, :profile_photo_url, presence: true
 
   validates :username, uniqueness: true
   validates :username, uniqueness: {case_insensitive: true}
@@ -24,12 +27,44 @@ class User < ActiveRecord::Base
   validates :username, format: {with: /\w/, message: "Usernames may only contain numbers, letters, or underscores."}
 
   validates :email, uniqueness: true
-  validates :email, uniqueness: {message: "That email is already registered."}
   validates :email, correct_email_format: true
 
   validates :password, length: {minimum: 5}
 
-  validates :country_id, numericality: {only_integer:true}
-  validates :verified, inclusion: { :in => [true, false]}
+  validates :country_id, numericality: {only_integer:true}, on: :update, :if => :entering_additional_info?
+
+  #inform model of two-step sign-up form
+
+  #allow access to write the current_step
+  attr_writer :current_step
+
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
+  #find the current step the user is on, or default to the first
+  def current_step
+    @current_step || steps.first
+  end
+
+  #move to next step
+  def next_step
+    self.current_step = steps[steps.index(current_step)+1]
+  end
+
+  #go back to last step
+  def previous_step
+    self.current_step=steps[steps.index(current_step)-1]
+  end
+
+  #array of possible steps, based on partial names
+  def steps
+    %w[sign_up additional_info]
+  end
 
 end
