79a2c8929402b97db6d76a5043f367e228b54dda
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-14 20:05:34 -0400

    add additional user seed

--- kitter/db/seeds.rb
+++ kitter/db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+    id: 5,
+    username: "joeschmoe",
+    name: "joe",
+    email: "joe@schmoe.com",
+    background_photo_url: "jpeg.jpeg",
+    profile_photo_url: "jpeg.jpg",
+    bio: "just yr avg joe",
+    website: "avejoe.com",
+    verified: true,
+    location: "NYC",
+    password: "factory88",
+    password_confirmation: "factory88",
+    country_id: 1)
+
+
+User.create(
+    id: 1,
+    username: "samschmoes",
+    name: "sam",
+    email: "sam@schmoe.com",
+    background_photo_url: "jpeg.jpeg",
+    profile_photo_url: "jpeg.jpg",
+    bio: "just yr avg sam",
+    website: "avesam.com",
+    verified: false,
+    location: "NYC",
+    password: "factory88",
+    password_confirmation: "factory88",
+    country_id: 1)
+
