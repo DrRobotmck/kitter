4f3bb9da93e8b2ca0c67e17f839dc6daa228bab0
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-15 02:51:04 -0400

    remove comments

--- kitter/app/models/retweet.rb
+++ kitter/app/models/retweet.rb
@@ -2,16 +2,3 @@ class Retweet < ActiveRecord::Base
   belongs_to :user
   belongs_to :tweet
 end
-
-
-
-# a retweet has the poster and the tweet it belongs to
-# a tweet has its own user
-# in order to call all the retweets of a specific user
-# find all tweets that have been retweeted
-# and go through and find tweets where their user_id is equal to the users id
-#
-#
-# Tweet.where(user_id: user.id)
-#
-# Retweet.where(tweet_id=)
